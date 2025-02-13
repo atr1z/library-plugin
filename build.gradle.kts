f2a07c3f5839c775955ff00dc36ae252eca781b1
atr1z <dev@atriz.com.mx>
2025-02-13 13:51:49 -0600

    actions fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
-import com.vanniktech.maven.publish.GradlePublishPlugin
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     kotlin("jvm") version "2.0.21"
     `kotlin-dsl`
-    `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "1.2.1"
-    id("com.vanniktech.maven.publish") version "0.28.0"
+    signing
+    id("eu.kakde.gradle.sonatype-maven-central-publisher") version "1.0.6"
 }
 
 group = "mx.com.atriz"
-version = "0.0.7"
 
 repositories {
     mavenCentral()
@@ -20,52 +15,70 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.android.tools.build:gradle:8.6.1")
+    implementation("com.android.tools.build:gradle:8.8.0")
 }
 
 kotlin {
     jvmToolchain(21)
 }
 
-gradlePlugin {
-    plugins {
-        create("library") {
-            id = "mx.com.atriz.library"
-            implementationClass = "mx.com.atriz.Library"
-            version = version
-            displayName = "Atriz Module Plugin"
-            description = "All needed setup for application development"
-        }
-    }
+object Meta {
+    val COMPONENT_TYPE = "java"
+    val GROUP = "mx.com.atriz"
+    val ARTIFACT_ID = "library"
+    val VERSION = "0.0.7"
+    val PUBLISHING_TYPE = "AUTOMATIC"
+    val SHA_ALGORITHMS = listOf("SHA-256", "SHA-512")
+    val DESC = "Module library for android applications"
+    val LICENSE = "Apache-2.0"
+    val LICENSE_URL = "https://opensource.org/licenses/Apache-2.0"
+    val GITHUB_REPO = "atr1z/library-plugin.git"
+    val DEVELOPER_ID = "atr1z"
+    val DEVELOPER_NAME = "Atriz"
+    val DEVELOPER_ORGANIZATION = "Atriz"
+    val DEVELOPER_ORGANIZATION_URL = "https://atriz.com.mx"
 }
 
-mavenPublishing {
-    configure(GradlePublishPlugin())
+sonatypeCentralPublishExtension {
+    groupId.set(Meta.GROUP)
+    artifactId.set(Meta.ARTIFACT_ID)
+    version.set(Meta.VERSION)
+    componentType.set(Meta.COMPONENT_TYPE)
+    publishingType.set(Meta.PUBLISHING_TYPE)
+    username.set(System.getenv("SONATYPE_USERNAME") ?: "")
+    password.set(System.getenv("SONATYPE_PASSWORD") ?: "")
     pom {
-        name.set("Application Plugin")
-        description.set("Application settings ready to build")
-        inceptionYear.set("2024")
-        url.set("https://github.com/atr1z/application-plugin/")
+        name.set(Meta.ARTIFACT_ID)
+        description.set(Meta.DESC)
+        url.set("https://github.com/${Meta.GITHUB_REPO}")
         licenses {
             license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                name.set(Meta.LICENSE)
+                url.set(Meta.LICENSE_URL)
             }
         }
         developers {
             developer {
-                id.set("atr1z")
-                name.set("Jair M.")
-                url.set("https://github.com/atr1z/")
+                id.set(Meta.DEVELOPER_ID)
+                name.set(Meta.DEVELOPER_NAME)
+                organization.set(Meta.DEVELOPER_ORGANIZATION)
+                organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
             }
         }
         scm {
-            url.set("https://github.com/atr1z/library-plugin/")
-            connection.set("scm:git:git://github.com/atr1z/library-plugin.git")
-            developerConnection.set("scm:git:ssh://git@github.com/atr1z/library-plugin.git")
+            url.set("https://github.com/${Meta.GITHUB_REPO}")
+            connection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
+            developerConnection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
+        }
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
         }
     }
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-    signAllPublications()
+}
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("SIGNING_KEY") ?: "",
+        System.getenv("SIGNING_PASSWORD") ?: ""
+    )
 }
