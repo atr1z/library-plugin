fdbcc2dd009e636fffef5d769d9dff99fde90570
atr1z <dev@atriz.com.mx>
2025-02-14 07:45:51 -0600

    switching to gradle-plugin-portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
+    signing
     kotlin("jvm") version "2.0.21"
     `kotlin-dsl`
     `java-gradle-plugin`
-    signing
-    id("eu.kakde.gradle.sonatype-maven-central-publisher") version "1.0.6"
+    id("com.gradle.plugin-publish") version "1.3.1"
 }
 
 group = "mx.com.atriz"
+version = "0.0.0"
 
 repositories {
     mavenCentral()
@@ -24,68 +25,16 @@ kotlin {
 }
 
 gradlePlugin {
+    website = "https://atriz.com.mx"
+    vcsUrl = "https://github.com/atr1z/library-plugin"
     plugins {
         create("library") {
             id = "mx.com.atriz.library"
             implementationClass = "mx.com.atriz.Library"
-            version = Meta.VERSION
-            displayName = "Atriz Module Plugin"
-            description = Meta.DESC
-        }
-    }
-}
-
-object Meta {
-    val COMPONENT_TYPE = "java"
-    val GROUP = "mx.com.atriz"
-    val ARTIFACT_ID = "library"
-    val VERSION = "0.0.8"
-    val PUBLISHING_TYPE = "AUTOMATIC"
-    val SHA_ALGORITHMS = listOf("SHA-256", "SHA-512")
-    val DESC = "Module library for android applications"
-    val LICENSE = "Apache-2.0"
-    val LICENSE_URL = "https://opensource.org/licenses/Apache-2.0"
-    val GITHUB_REPO = "atr1z/library-plugin.git"
-    val DEVELOPER_ID = "atr1z"
-    val DEVELOPER_NAME = "Atriz"
-    val DEVELOPER_ORGANIZATION = "Atriz"
-    val DEVELOPER_ORGANIZATION_URL = "https://atriz.com.mx"
-}
-
-sonatypeCentralPublishExtension {
-    groupId.set(Meta.GROUP)
-    artifactId.set(Meta.ARTIFACT_ID)
-    version.set(Meta.VERSION)
-    componentType.set(Meta.COMPONENT_TYPE)
-    publishingType.set(Meta.PUBLISHING_TYPE)
-    username.set(System.getenv("SONATYPE_USERNAME") ?: "")
-    password.set(System.getenv("SONATYPE_PASSWORD") ?: "")
-    pom {
-        name.set(Meta.ARTIFACT_ID)
-        description.set(Meta.DESC)
-        url.set("https://github.com/${Meta.GITHUB_REPO}")
-        licenses {
-            license {
-                name.set(Meta.LICENSE)
-                url.set(Meta.LICENSE_URL)
-            }
-        }
-        developers {
-            developer {
-                id.set(Meta.DEVELOPER_ID)
-                name.set(Meta.DEVELOPER_NAME)
-                organization.set(Meta.DEVELOPER_ORGANIZATION)
-                organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
-            }
-        }
-        scm {
-            url.set("https://github.com/${Meta.GITHUB_REPO}")
-            connection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
-            developerConnection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
-        }
-        issueManagement {
-            system.set("GitHub")
-            url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
+            displayName = "Atriz Library Plugin"
+            version = project.version
+            description = "Android module library ready to use with custom configurations"
+            tags = listOf("atriz", "library-plugin", "android")
         }
     }
 }
@@ -95,4 +44,5 @@ signing {
         System.getenv("SIGNING_KEY") ?: "",
         System.getenv("SIGNING_PASSWORD") ?: ""
     )
+    sign(configurations.runtimeElements.get())
 }
